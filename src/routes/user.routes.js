fae03959cb2c47f2f5ddc7ea9054c9808ee40b94
Talat Anik <talat_anik@hotmail.com>
2024-12-09 03:34:45 +0600

    added endpoint to get user info

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model");
 
-// Example: Fetch all users
-router.get("/", (req, res) => {
-  res.json({ message: "User routes working!" });
+
+router.get("/:instructorId", async (req, res) => {
+  const { instructorId } = req.params;
+
+  try {
+    // Find instructor by ID
+    const instructor = await User.findById(instructorId, "name");
+
+    if (!instructor) {
+      return res.status(404).json({ message: "Instructor not found" });
+    }
+
+    res.status(200).json({
+      instructorId: instructor._id,
+      name: instructor.name,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
